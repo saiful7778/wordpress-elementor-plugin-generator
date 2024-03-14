b66b7e81e25b8c2ecc42695ec05c0846a7f33975
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-15 00:25:03 +0600

    change layout

--- src/app/plugin/layout.jsx
+++ src/app/plugin/layout.jsx
@@ -2,16 +2,14 @@ import ResizeBar from "@/components/ResizeBar";
 
 const PluginLayout = ({ settings, preview, code }) => {
   return (
-    <div className="flex h-screen w-full flex-col overflow-auto lg:flex-row">
-      <div className="flex w-full flex-col lg:w-3/5">
-        <div className="h-full overflow-auto">{settings}</div>
+    <div className="flex h-screen w-full flex-col overflow-auto">
+      <div className="flex h-3/5 w-full">
+        <div className="w-3/5 overflow-auto">{settings}</div>
+        <ResizeBar target="col" />
+        <div className="w-2/5 overflow-auto">{preview}</div>
       </div>
-      <ResizeBar target="col" />
-      <div className="flex w-full flex-col lg:w-2/5">
-        <div className="h-1/2 overflow-auto">{preview}</div>
-        <ResizeBar target="row" />
-        <div className="h-1/2 overflow-auto">{code}</div>
-      </div>
+      <ResizeBar target="row" />
+      <div className="h-2/5 w-full overflow-auto">{code}</div>
     </div>
   );
 };
